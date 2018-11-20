9b59545d099c8fba078d15d986e9138dce2a835e
Pradeep Kumar Aitha <paitha@buffalo.edu>
2018-11-20 16:54:23 -0500

    daily routine api integration

--- app-directives/daily-balance-chart/daily-balance-chart.js
+++ app-directives/daily-balance-chart/daily-balance-chart.js
@@ -31,8 +31,40 @@
                 }
 
                 function submitDailyBalance() {
-                    
-                    vm.dailyBalanceSubmitted = true;
+                    var entries_mod = [];
+                    for (var i = 0; i < vm.dailyBalanceEntries.length; i++) {
+                        var entry = vm.dailyBalanceEntries[i];
+                        if (entry.balanceChartName === 'q1' || entry.balanceChartName === 'q2') {
+                            vm.overallScore += entry.score;
+                        }
+                        var entry_mod = {
+                            balanceChartName: entry.balanceChartName,
+                            description: entry.description,
+                            score: entry.score
+                        };
+                        entries_mod.push(entry_mod);
+                    }
+
+                    var payload = {
+                        email: AppGlobalConstants.userData.email,
+                        balanceChart: entries_mod
+                    };
+
+                    $http({
+                        url: AppGlobalConstants.baseURL + '/dailyRoutine',
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        data: payload
+                    })
+                    .then(
+                        function(response) {
+                            vm.dailyBalanceSubmitted = true;
+                        },
+                        function(response) {
+                            // show an error message
+                        }
+                    )
+
                 }
 
                 function fieldChanged() {
