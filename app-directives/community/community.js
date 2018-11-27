85d689c7e9bd66b2ce1af9696aba042db1d83ad3
sourabh <sourabhb@buffalo.edu>
2018-11-27 12:30:01 -0500

    addded communiuty

---
+++ app-directives/community/community.js
@@ -0,0 +1,135 @@
+(function () {
+    'use strict';
+
+    angular
+        .module('app')
+        .directive('appCommunity', Community);
+
+    Community.$inject = ['$http', 'AppGlobalConstants', '$rootScope'];
+    function Community($http, AppGlobalConstants, $rootScope) {
+        return {
+            restrict: 'EA',
+            templateUrl: './app-directives/community/community.html',
+            link: function (scope, element, attrs) {
+                var vm = scope;
+
+                vm.lessons = null;
+                vm.lesson1Submitted = false;
+                vm.lesson3Submitted = false;
+
+                vm.showWeeklyOverview = true;
+                vm.showLessonOne = false;
+                vm.showLessonTwo = false;
+                vm.showLessonThree = false;
+
+                vm.openLesson = openLesson;
+                vm.switchToLessonsOverview = switchToLessonsOverview;
+                vm.submitLesson = submitLesson;
+                vm.switchToDailyBalanceChart = switchToDailyBalanceChart;
+
+                function fetchLessons(){
+                    $http({
+                        url: AppGlobalConstants.baseURL + '/lesson?page=1 ',
+                        method: "GET",
+                        headers: {"Content-Type": "application/json"},
+                    })
+                    .then( function(response) {
+                        vm.lessons = response.data;
+                        var lesson3 = vm.lessons[1];
+                        vm.lessons.splice(1, 1);
+                        vm.lessons.push(lesson3);
+                        // prefill previous responses
+                        evalLessonsResponse();
+                    })
+                    .catch(angular.noop);
+                }
+
+                function openLesson(num) {
+                    vm.showWeeklyOverview = false;
+                    if (num === 1) {
+                        vm.showLessonOne = true;
+                    }
+                    else if (num ===2) {
+                        vm.showLessonTwo = true;
+                    }
+                    else if (num === 3) {
+                        vm.showLessonThree = true;
+                    }
+                }
+
+                function switchToLessonsOverview() {
+                    vm.showWeeklyOverview = true;
+                    vm.showLessonOne = false;
+                    vm.showLessonTwo = false;
+                    vm.showLessonThree = false;
+                }
+
+                function submitLesson(num){
+                    // check for invalid response
+                    var invalid = false;
+                    var tasks = vm.lessons[num-1].tasks;
+                    for (var i = 0; i < tasks.length; i++) {
+                        if (tasks[i].userResponse.trim() === ""){
+                            tasks[i].invalidResponse = true;
+                            invalid = true;
+                        }
+                        else {
+                            tasks[i].invalidResponse = false;
+                        }
+                    }
+                    if (invalid) {
+                        return;
+                    }
+                    // submit api
+                    var payload = {
+                        lesson: vm.lessons[0],
+                        email: AppGlobalConstants.userData.email
+                    };
+                    $http({
+                        url: AppGlobalConstants.baseURL + '/lesson',
+                        method: "POST",
+                        headers: {"Content-Type": "application/json"},
+                        data: payload
+                    })
+                    .then(function(response) {
+                        if (num === 1) {
+                            vm.lesson1Submitted = true;
+                        }
+                        else if (num === 3) {
+                            vm.lesson3Submitted = true;
+                        }
+                    })
+                    .catch(angular.noop);
+                }
+
+                function switchToDailyBalanceChart() {
+                    $rootScope.$emit('switchToDailyBalanceChart');
+                    switchToLessonsOverview();
+                }
+
+                function evalLessonsResponse() {
+                    var userData = AppGlobalConstants.userData;
+                    var lessonResponses = userData.lessonResponses;
+                    for (var i = 0; i < lessonResponses.length; i++) {
+                        var response = lessonResponses[i];
+                        if (!response.lessonSubmitted) {
+                            continue;
+                        }
+                        if (response.lessonNumber === 1) {
+                            vm.lesson1Submitted = true;
+                            vm.lessons[0].tasks = response.tasks;
+                        }
+                        else if (response.lessonNumber === 3) {
+                            vm.lesson3Submitted = true;
+                            vm.lessons[2].tasks = response.tasks;
+                        }
+                    }
+                }
+
+                vm.$on('fetchUserDataComplete', fetchLessons);
+            }
+        }
+    }
+
+
+})();
--- home/home.controller.js
+++ home/home.controller.js
@@ -13,17 +13,20 @@
         vm.showLesson = false;
         vm.showQuestionnaire = false;
         vm.showDailyBalanceChart = false;
-        
+        vm.showCommunity = false;
+
         vm.goToQuestionnaire = goToQuestionnaire;
         vm.goToDailyBalanceChart = goToDailyBalanceChart;
         vm.goToDashboard = goToDashboard;
         vm.goToLesson = goToLesson;
+        vm.goToCommunity = goToCommunity;
 
         function goToQuestionnaire(){
             vm.showDashboard = false;
             vm.showLesson = false;
             vm.showDailyBalanceChart = false;
             vm.showQuestionnaire = true;
+            vm.showCommunity = false;
         }
 
         function goToDailyBalanceChart() {
@@ -31,6 +34,7 @@
             vm.showLesson = false;
             vm.showQuestionnaire = false;
             vm.showDailyBalanceChart = true;
+            vm.showCommunity = false;
         }
 
         function goToDashboard() {
@@ -38,6 +42,7 @@
             vm.showQuestionnaire = false;
             vm.showDailyBalanceChart = false;
             vm.showDashboard = true;
+            vm.showCommunity = false;
         }
 
         function goToLesson() {
@@ -45,6 +50,15 @@
             vm.showQuestionnaire = false;
             vm.showDailyBalanceChart = false;
             vm.showLesson = true;
+            vm.showCommunity = false;
+        }
+
+        function goToCommunity() {
+            vm.showDashboard = false;
+            vm.showQuestionnaire = false;
+            vm.showDailyBalanceChart = false;
+            vm.showLesson = false;
+            vm.showCommunity = true;
         }
 
         $rootScope.$on('switchToDailyBalanceChart', goToDailyBalanceChart);
